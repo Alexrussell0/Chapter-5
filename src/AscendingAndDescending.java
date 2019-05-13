cf2d9e2c236d7bf8c0dc542d1f09b40932c56f71
Alex Russell <alexrussellpersonal@gmail.com>
2019-05-13 12:24:41 -0500

    Update

---
+++ src/AscendingAndDescending.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AscendingAndDescending {
+    public static void main(String[] args)
+    {
+        int userA;
+        int userB;
+        int userC;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("welcome to my game. \n");
+        System.out.println("Its called Ascending And Descending\n");
+        userA = input.nextInt();
+        System.out.println("Enter a whole number >>>.");
+        userB = input.nextInt();
+        System.out.println("Enter a whole number >>>.");
+        userC = input.nextInt();
+
+        if (userA > userB){
+            if(userB > userC){
+                if(userB > userC);
+            }
+        }
+
+    }
+}
---
+++ src/HighLow.java
@@ -0,0 +1,8 @@
+public class HighLow {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to my game.");
+        System.out.println("pick a number between 1 to 10.\n");
+
+    }
+}
